4bbb736ef0b77e9f1608140e07b661fb723a42b0
lostark13 <tushar9915@gmail.com>
2022-03-01 12:52:54 -0300

    raphael: parts: Add per app maximum refresh rate
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,6 +24,7 @@ import android.util.Log;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
+import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -41,5 +42,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             Log.d(TAG, "Dirac is not present in system");
         }
         PopupCameraUtils.startService(context);
+        RefreshUtils.startService(context);
     }
 }
