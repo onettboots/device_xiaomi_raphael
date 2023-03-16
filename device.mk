88106d6e61d8a898d448805c11e3c71918392c71
onettboots <blackcocopet@gmail.com>
2023-03-16 16:28:48 +0000

    add new LED from davinci
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- device.mk
+++ device.mk
@@ -297,7 +297,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.raphael
+    android.hardware.light@2.0-service.xiaomi_sm8150
 
 # Livedisplay
 PRODUCT_PACKAGES += \
