e8e5a348d4793f456da77b88d7917ad3617d95d8
onettboots <blackcocopet@gmail.com>
2023-03-21 14:59:53 +0000

    raphael: Add new light, testing
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- device.mk
+++ device.mk
@@ -300,7 +300,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light@2.0-service.xiaomi_raphael
 
 # Livedisplay
 PRODUCT_PACKAGES += \
