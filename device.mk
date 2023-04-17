e75e227a2f122c2a431f71d463e029c5bfa3edc4
onettboots <blackcocopet@gmail.com>
2023-04-17 07:53:41 +0000

    Revert "raphael: Introduce missing thermal engine configs and Limits HAL"
    This reverts commit 738bdc6e251130b37df06dd4b94ab8112642fbd0.

--- device.mk
+++ device.mk
@@ -516,10 +516,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0.vendor
-
 # Tetheroffload
 PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
