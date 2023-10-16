969be42359efafad657a1db8e3a558af16c3c564
Pranav Vashi <neobuddy89@gmail.com>
2023-10-16 22:47:27 +0530

    raphael: Compile libprotobuf-cpp-lite-3.9.1-vendorcompat for legacy blobs
    Change-Id: Ie69d6f3592c665abd2d144bceecbcd51e23eddc4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- device.mk
+++ device.mk
@@ -444,6 +444,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 # QTI
 PRODUCT_PACKAGES += \
     libjson \
