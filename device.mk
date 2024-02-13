b9a1047cc37415971a5cc7d1ee8d7d913057541f
Cyber Knight <cyberknight755@gmail.com>
2024-02-13 08:15:32 +0000

    raphael: move Enable FUSE Passthrough
    - The prop is supposed to be set in product instead of system as per [1].
    
    [1]: https://source.android.com/docs/core/storage/fuse-passthrough
    
    Change-Id: I4c467da6b1a659625aa20e79a718736b2ee9f915
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- device.mk
+++ device.mk
@@ -237,6 +237,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
+# FUSE passthrough
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.fuse.passthrough.enable=true
+
+
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
