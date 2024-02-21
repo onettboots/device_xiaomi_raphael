a9b0497d713a9adf4284d4488adbd609a74f90d5
firebird11 <hbgassel@gmail.com>
2024-02-21 10:39:43 +0100

    raphael: Import powerhint.json
    * taken from: https://github.com/xiaomi-sm8150-devs/android_device_xiaomi_sm8150-common
    
    Change-Id: Idc56d7a351c3ff8c99947abfc59ab1d6243bba8c
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- device.mk
+++ device.mk
@@ -429,6 +429,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/msmnile/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QTI
