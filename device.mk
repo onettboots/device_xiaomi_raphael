1057e1ea5712841650d07d9f992ae19813f45eee
onettboots <blackcocopet@gmail.com>
2024-03-22 19:23:37 +0000

    Revert "raphael: Switch to pixel libperfmr"
    This reverts commit 1597ad0a13e11a22d9276b88c36adc3af59f26d5.

--- device.mk
+++ device.mk
@@ -442,6 +442,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/msmnile/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/power/config/msmnile/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # QTI
 PRODUCT_PACKAGES += \
     libjson \
