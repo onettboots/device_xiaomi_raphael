ee9ada828d00a3bcc337d7c1a0f522bddf7c2cdd
onettboots <blackcocopet@gmail.com>
2024-07-25 09:25:09 +0000

    Revert "raphael: Enable casefolding support on userdata"
    This reverts commit 246fb9a456d70855949042deba8706b0858bdd8b.

--- device.mk
+++ device.mk
@@ -23,9 +23,6 @@ $(call inherit-product, vendor/xiaomi/raphael/raphael-vendor.mk)
 # MiuiCamera
 $(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
