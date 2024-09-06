884786477d7a8d0d90f95540f3c9293604b60b8a
onettboots <blackcocopet@gmail.com>
2024-09-06 14:57:20 +0000

    Revert "raphael: Move to QTI health AIDL service"
    This reverts commit 4edcfd1171b88ae0b763d33457b1a8789ff05d1c.

--- device.mk
+++ device.mk
@@ -262,9 +262,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti \
-    android.hardware.health-service.qti_recovery \
-    android.hardware.health@2.1.vendor
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # Storage health HAL
 PRODUCT_PACKAGES += \
