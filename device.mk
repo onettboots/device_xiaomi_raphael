576e63b37d6849246a3b7117be42876e3d1403ce
onettboots <blackcocopet@gmail.com>
2024-09-20 12:24:24 +0000

    Revert "raphael: Move to health AIDL services"
    This reverts commit f8654bd0d43592c629ebaeb006e8beb5deee6cd4.

--- device.mk
+++ device.mk
@@ -297,8 +297,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.example \
-    android.hardware.health-service.example_recovery
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1.vendor
